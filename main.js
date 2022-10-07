9eaac20b5616071911c982cb06dd0c31fe7822bb
RodrigoC <rodrigocardona87@gmail.com>
2022-10-07 17:58:48 -0300

    funcionamiento de carrito de compras y menu

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
+// .Variables
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const menuHamIcon = document.querySelector('#menuHamIcon');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+// .Eventos
 menuEmail.addEventListener('click', toogleDesktopMenu);
-
+menuHamIcon.addEventListener('click', toogleMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
+// .Funciones
 function toogleDesktopMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
   desktopMenu.classList.toggle('inactive');
+}
+function toogleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+  mobileMenu.classList.toggle('inactive');
+}
+function toogleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+  aside.classList.toggle('inactive');
 }
